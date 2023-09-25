81a30138d6004b720b447bf142a58c769a50b8fe
mailism <domiam09@gmail.com>
2023-09-25 15:15:43 +0700

    ft_printf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -1,18 +1,59 @@
 #include "libft.h"
 #include <stdlib.h>
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (s == NULL)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putnbr_fd(-214748364, fd);
+		ft_putchar_fd(8 + '0', fd);
+		return ;
+	}
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+	}
+	if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + '0', fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
+}
+
 int	ft_format(va_list args, const char format)
 {
 	void	*ptr;
 
 	if (format == 'c')
-		return (ft_putchar((va_arg(args, int))));
+		return (ft_putchar_fd((va_arg(args, int))));
 	else if (format == 's')
-		return (ft_putstr((va_arg(args, char *))));
+		return (ft_putstr_fd((va_arg(args, char *))));
 	else if (format == 'd' || format == 'i')
-		return (ft_putnbr((va_arg(args, int))));
+		return (ft_putnbr_fd((va_arg(args, int))));
 	else if (format == '%')
-		return (ft_putchar('%'));
+		return (ft_putchar_fd('%'));
 	else
 		return (-1);
 }
